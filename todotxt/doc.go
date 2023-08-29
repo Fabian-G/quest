7cff45be54726626f2eda711a51685ad07fd8726
Fabian-G <fabian_g@mailbox.org>
2023-08-29 10:08:32 +0200

    Add doc.go

---
+++ todotxt/doc.go
@@ -0,0 +1,5 @@
+// The purpose of this package is to provide an API that
+// strictly complies with the todotxt spec.
+// In particular this means that any non-standard behavior (including recurring tasks)
+// does not belong here, but in the extensions package
+package todotxt
